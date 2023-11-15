cdc6eecc97b6e4dab4db8f0409cd8f0f95715530
leeny_the_bear <leeny@inbox.ru>
2023-11-15 12:25:56 +0300

    configure project settings

--- mentor_and_students/settings.py
+++ mentor_and_students/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i#l^+9wt%=el#h34dbw+ah0ou=)1psi!+(u$t6ifih*6%$(ryi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
