c9f0bf3ef1c09cd1a8841af45e5f3b679e515c3d
leeny_the_bear <leeny@inbox.ru>
2023-11-30 12:45:32 +0300

    add buttons for main menu

--- notifications/keyboards.py
+++ notifications/keyboards.py
@@ -10,10 +10,11 @@ def main_menu_keyboard():
 
 
 def future_lessons_keyboard():
-        keyboard = [
-            [InlineKeyboardButton("Посмотреть уроки сегодня", callback_data='today')],
-            [InlineKeyboardButton("Посмотреть уроки завтра", callback_data='tomorrow')],
-            [InlineKeyboardButton("Посмотреть уроки на 3 дня", callback_data='3_days')]
-                    ]
+    keyboard = [[InlineKeyboardButton("Посмотреть уроки сегодня", callback_data='today')],
+                [InlineKeyboardButton("Посмотреть уроки завтра", callback_data='tomorrow')],
+                [InlineKeyboardButton("Посмотреть уроки послезавтра", callback_data='2_days')],
+                [InlineKeyboardButton("Главное меню", callback_data="main_menu")]]
+
+    return InlineKeyboardMarkup(keyboard)
 
         return InlineKeyboardMarkup(keyboard)
